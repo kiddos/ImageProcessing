64a566083a6fe4a0384f5d1986324935bfebbd38
kiddos <josephyu831007@yahoo.com.tw>
2016-11-29 16:44:43 +0800

    change pattern3 to tplzp

--- web/node/public/javascripts/lzp-main.js
+++ web/node/public/javascripts/lzp-main.js
@@ -7,7 +7,7 @@ $(document).ready(function() {
   $('#spinner').html(
     '<option value="/javascripts/lzp-pattern1.js">lzp pattern 1</option>' +
     '<option value="/javascripts/lzp-pattern2">lzp pattern 2</option>' +
-    '<option value="/javascripts/lzp-pattern3.js">lzp pattern 3</option>'
+    '<option value="/javascripts/tplzp.js">tplzp</option>'
   );
   // setup editor
   initEditor();
